9f24f3375fed2e604615726ce89553494e14c70d
Shogun128 <69772843+Shogun128@users.noreply.github.com>
2022-09-16 06:36:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>hi there</p>
+    <?php
+    echo "hellon horld";
+    ?>
+  </body>
+</html>
 
